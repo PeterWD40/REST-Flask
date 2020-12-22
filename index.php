15d212f939d485be1de5e1ad3719452a30696384
CDA <cda@MacBook-Pro.local>
2020-12-22 10:22:43 -0500

    testing

--- index.php
+++ index.php
@@ -1,1 +1,11 @@
 <?php include_once(“home.html”); ?>
+
+<!DOCTYPE html>
+<html>
+     <head>
+          <title>My Website Title</title>
+     </head>
+     <body>
+          PeterWD40 API
+     </body>
+</html>
